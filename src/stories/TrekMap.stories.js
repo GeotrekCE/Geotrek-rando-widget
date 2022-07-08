c105c13a3855885535ad91f9eb37b95938733149
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2022-07-08 12:54:28 +0200

    Add IGN base layer - Fix typo

--- src/stories/TrekMap.stories.js
+++ src/stories/TrekMap.stories.js
@@ -5,7 +5,7 @@ export default {
   argTypes: {
     urlLayer: {
       control: 'select',
-      options: ['openstreetmap', 'planignv2],
+      options: ['openstreetmap', 'planignv2'],
       mapping: {
         openstreetmap: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
         planignv2: 'https://wxs.ign.fr/cartes/geoportail/wmts?&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE=normal&TILEMATRIXSET=PM&FORMAT=image/png&LAYER=GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
